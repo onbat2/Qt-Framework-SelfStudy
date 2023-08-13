71b3023a4b3a6e9880cb38af00064c5901dadc03
onbat2 <quangduc11202@gmail.com>
2023-08-13 14:58:40 +0700

    [UPDATE]

--- Mini Projects/build-Image-Viewer-Desktop_Qt_5_15_2_GCC_64bit-Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,7 +0,0 @@
-# Additional clean files
-
-file(REMOVE_RECURSE
-  "CMakeFiles/Image-Viewer_autogen.dir/AutogenUsed.txt"
-  "CMakeFiles/Image-Viewer_autogen.dir/ParseCache.txt"
-  "Image-Viewer_autogen"
-)
